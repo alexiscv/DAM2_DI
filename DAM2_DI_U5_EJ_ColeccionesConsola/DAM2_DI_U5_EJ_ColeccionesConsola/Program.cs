e5ae295e1a2f13a8bd883de67610e46b7e140dbb
alexiscv <hogo.jp@gmail.com>
2018-01-14 11:58:30 +0100

    App Diccionario en C#
    Ejercicio de crear un diccionario usando la Collection Dictionary en consola.

---
+++ DAM2_DI_U5_EJ_ColeccionesConsola/DAM2_DI_U5_EJ_ColeccionesConsola/Program.cs
@@ -0,0 +1,53 @@
+﻿using DAM2_DI_U5_EJ_ColeccionesConsola.logica;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAM2_DI_U5_EJ_ColeccionesConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool salir = false;
+
+            // Mostrar menú
+            while (!salir)
+            {
+
+                // Mostramos el menú
+                Logica.mostrarMenu();
+
+                // Capturamos y ejecutamos la opción elegida
+                int op = Convert.ToInt32(System.Console.ReadLine());
+
+                switch (op)
+                {
+                    case 1:
+                        Logica.addTermino();
+                        break;
+
+                    case 2:
+                        Logica.buscarDefinicion();
+                        break;
+
+                    case 3:
+                        Logica.listarDefiniciones();
+                        break;
+
+                    case 4:
+                        salir = true;
+                        break;
+
+                    default:
+                        break;
+                }
+
+            }
+
+        }
+    }
+}
---
+++ DAM2_DI_U5_EJ_ColeccionesConsola/DAM2_DI_U5_EJ_ColeccionesConsola/logica/Logica.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAM2_DI_U5_EJ_ColeccionesConsola.logica
+{
+    public static class Logica
+    {
+
+        // Atributos
+        private static Dictionary<string, string> diccionario = new Dictionary<string, string>();
+
+        // Métodos
+        public static void addTermino()
+        {
+            // Donde almacenaremos la clave/valor
+            String clave = "";
+            String valor = "";
+
+            // Solitamos la clave y el valor
+            Console.WriteLine("Introduce una clave para el término:");
+            clave = Console.ReadLine();
+
+            Console.WriteLine("Introduce la definición para el término:");
+            valor = Console.ReadLine();
+
+            // Intentamos añadir
+            // Comprobando que no exista ya la clave en la coleccion
+            if( !clave.Equals("") && !valor.Equals("") )
+            {
+                try
+                {
+                    diccionario.Add(clave, valor);
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("Ya existe un elemento con la clave = \"clave\".");
+                }
+            }
+            else{
+                Console.WriteLine("Error: Ni la clave ni el valor pueden estar vacios. Vuelva a intentar añadir el termino.");
+            }
+
+        }
+
+        public static void buscarDefinicion()
+        {
+
+            // Donde almacenaremos la clave/valor
+            String clave = "";
+            String valor = "";
+
+            // Solitamos la clave para poder buscar el valor
+            Console.WriteLine("¿Qué palabra desea buscar?:");
+            clave = Console.ReadLine();
+
+            // Buscamos el valor
+            if (diccionario.TryGetValue(clave, out valor))
+            {
+                Console.WriteLine("--> La definición de: \"{0}\", es = {1}.", clave, valor);
+            }
+            else
+            {
+                Console.WriteLine("Ups: La Clave \"{0}\" no ha sido encontrada.", clave);
+            }
+
+        }
+
+        public static void listarDefiniciones()
+        {
+
+            foreach (KeyValuePair<string, string> termino in diccionario)
+            {
+
+                Console.WriteLine("--> Clave = {0}, Valor = {1}", termino.Key, termino.Value);
+                
+            }
+
+        }
+
+        public static void mostrarMenu()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("#### MENU ####");
+            Console.WriteLine("1) Añadir un termino al diccionario");
+            Console.WriteLine("2) Buscar un termino");
+            Console.WriteLine("3) Enumerar terminos");
+            Console.WriteLine("4) Salir");
+            Console.WriteLine("");
+        }
+
+    }
+}
